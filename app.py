43d2cbfcccac912ac7b3a46d4c501dbc473fb4c6
vovakenzov30 <yura.volohin@gmail.com>
2023-02-17 21:20:08 +0200

    make it better

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
-bot = Bot(token='6255668864:AAEFnaYJ83hvYgkTUa411QF5xpAxYh5NPok')
+import os
 
+from aiogram import Bot, Dispatcher, types, executor
+import config
+
+# from aiogram.utils import executor
+bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
+
+
+async def on_startup(dp):
+    await bot.set_webhook(config.URL)
+
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
 
 
 @dp.message_handler()
@@ -12,4 +23,12 @@ async def get_message(message: types.Message):
 
     await bot.send_message(chat_id=chat_id, text=text)
 
-executor.start_polling(dp)
+executor.start_webhook(
+    dispatcher=dp,
+    webhook_path='',
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    skip_updates=True,
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT"), 5000)
+)
